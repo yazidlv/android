173f7afd5e901790152e317bc3c940a8179f3160
lebsir <yasid.lebsir@imerir.com>
2013-06-20 22:20:37 +0200

    Update Requet.java

--- Trombi/src/com/lebsir/trombi/Requet.java
+++ Trombi/src/com/lebsir/trombi/Requet.java
@@ -37,31 +37,10 @@ public class Requet extends GererBDD{
 			ContentValues cv = new ContentValues();
 			cv.put(NOM, profil.getNom());
 			cv.put(PRENOM, profil.getPrenom());
-			
 			cv.put(IMAGE, profil.getImage());
 			BDD.insert(Requet.TABLE_NAME, null, cv);
 		}
-		
-		/*public void supprimer(profil profil){
-			BDD.delete(TABLE_NAME, ID + " = ?", new String[] {String.valueOf(profil.getId())});	
-		}
-		
-		
-		// option modifier je l'utilise pas 
-		/*public void modifier(profil profil){
-			ContentValues cv = new ContentValues();
-			cv.put(NOM, profil.getNom());
-			cv.put(PRENOM, profil.getPrenom());
-			cv.put(IMAGE,profil.getImage());
-			BDD.update(TABLE_NAME, cv,ID + " = ?", new String[] {String.valueOf(profil.getId())});	
-		}*/
-		
-		/*public profil selectionnerUnProfil(int id){
-			 Cursor c = BDD.rawQuery("SELECT "+ID+", "+NOM+", "+PRENOM+","+IMAGE+" FROM "+TABLE_NAME+" WHERE "+ID+"=?;", new String[] {String.valueOf(id)});
-			  c.moveToFirst();
-			return new Profil(c.getInt(0),c.getString(1),c.getString(2), c.getString(3),c.getString(4));
-		}*/
-		
+
 		public Cursor tousLesProfils(){
 			 Cursor c = BDD.rawQuery("SELECT "+ID+", "+NOM+", "+PRENOM+", "+IMAGE+" FROM "+TABLE_NAME+";", null);
 			return c;
